4011bee8885ce067a649b3c6db6e61095bf306a5
Pankaj Kumar <121335947+pkthapliyal@users.noreply.github.com>
2023-08-21 18:02:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const connection = mongoose.connect(process.env.mongoURL);
+const connection = mongoose.connect("mongodb+srv://pkthapliyal:pankajkr@cluster0.l1f5yob.mongodb.net/masai-hospital?retryWrites=true&w=majority");
 
 module.exports = { connection }
